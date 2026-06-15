18a95d6d4d108050d28229960b25a3d8973699c1
MatteoMiguelez <matteo.miguelez@gmail.com>
2026-06-15 19:30:16 +0100

    feat: mutualize career components

--- src/home/components/career/Career.vue
+++ src/home/components/career/Career.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-import ProfessionalCareer from '@/home/components/career/ProfessionalCareer.vue';
-import SchoolCareer from '@/home/components/career/SchoolCareer.vue';
+import CareerSection from '@/home/components/career/CareerSection.vue';
+import { SCHOOL_CAREER_LIST } from '@/home/constants/school-career-list.ts';
+import { PROFESSIONAL_CAREER_LIST } from '@/home/constants/professional-career-list.ts';
+
 </script>
 
 <template>
   <div class="career">
-    <ProfessionalCareer></ProfessionalCareer>
-    <SchoolCareer></SchoolCareer>
+    <CareerSection
+        title="Parcours Professionnel"
+        description="Au cours de mes études, je me suis progressivement inséré dans le monde professionnel : d'abord via un stage en deuxième année de BUT, puis en alternance pour ma troisième année. À l'issue de cette alternance, j'ai été embauché en CDI dans la même entreprise, ce qui m'a permis de poursuivre les projets déjà engagés tout en contribuant à de nouvelles missions."
+        :items="PROFESSIONAL_CAREER_LIST"
+    />
+    <CareerSection
+        title="Parcours Scolaire"
+        description="Dès le lycée, je me suis orienté vers une option informatique en seconde, puis la spécialité Numérique et Sciences Informatiques en première et terminale.
+      J'ai appris les bases du développement, que j'ai pu enrichir lors de mon Bachelor Universitaire et Technologique (BUT) Informatique."
+        :items="SCHOOL_CAREER_LIST"
+    />
   </div>
 </template>
 
---
+++ src/home/components/career/CareerSection.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import SectionTitle from '@/home/components/SectionTitle.vue';
+import TimeLine from '@/home/components/career/TimeLine.vue';
+import type { TimeLineItem } from '@/home/types/time-line-item.ts';
+
+defineProps<{
+  title: string;
+  description: string;
+  items: TimeLineItem[];
+}>();
+</script>
+
+<template>
+  <div class="career-section">
+    <div class="career-section__data">
+      <SectionTitle :title="title" />
+      <span class="career-section__description">{{ description }}</span>
+    </div>
+    <TimeLine :items="items" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.career-section {
+  display: flex;
+  gap: 98px;
+
+  @media (max-width: 1100px) {
+    flex-wrap: wrap;
+  }
+
+  @media (max-width: 768px) {
+    gap: 12px;
+
+    &__description {
+      display: none;
+    }
+  }
+
+  &__data {
+    display: flex;
+    flex-direction: column;
+    max-width: 300px;
+  }
+}
+
+:deep(.p-timeline-event-opposite) {
+  display: none !important;
+}
+
+:deep(.p-timeline-event-content) {
+  padding-bottom: 24px !important;
+}
+</style>
